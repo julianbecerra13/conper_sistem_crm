5801b16961e7a1ff2223c11ab3e530ff1ed808d0
Jairo Sanchez <jairosr_1995@hotmail.com>
2023-07-02 12:49:34 -0500

    working

--- conper_go/src/config/config.go
+++ conper_go/src/config/config.go
@@ -4,17 +4,11 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectDB() *gorm.DB {
-	errorENV := godotenv.Load()
-	if errorENV != nil {
-		panic("Error loading .env file")
-	}
-
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASS")
 	dbHost := os.Getenv("DB_HOST")
