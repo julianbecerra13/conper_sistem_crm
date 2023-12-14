f8a356611814b2abfbbe8b2df67539786f71f17f
perritwiguer130623 <becerrarodriguezjulian@gmail.com>
2023-12-14 18:05:38 -0500

    arreglos mantenimiento

--- flutter/lib/views/administrador/views/components/modaladm.dart
+++ flutter/lib/views/administrador/views/components/modaladm.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:conper/views/components/tabla.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vrouter/vrouter.dart';
 
 class MyModalContentadm extends StatelessWidget {
